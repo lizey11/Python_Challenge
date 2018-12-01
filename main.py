d51e3dd39327cc6bad57c17207f4435ad0c2bb21
Liza Hartlaub <42218054+lizey11@users.noreply.github.com>
2018-12-01 14:13:59 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,54 +1,49 @@
 import os
 import csv
-PyPoll= os.path.join('election_data.csv')
+PyBank= os.path.join('budget_data.csv')
 
 #open and read csv file
 
-with open(PyPoll, newline='') as csvfile:
+with open(PyBank, newline='') as csvfile:
 
     csvreader=csv.reader(csvfile, delimiter=',')
     csv_header = next(csvfile)
 
-   #Create lists to store results
-    voterid = []
-    county = []
-    candidates = []
-    
+    # create lists to hold profit and months
+    Profit = []
+    months = []
+
     #read through each row of data after header
     for rows in csvreader:
-        voterid.append(rows[0])
-        county.append(rows[1])
-        candidates.append(rows[2])
+        Profit.append(int(rows[1]))
+        months.append(rows[0])
+
+    # find revenue change
+    revenue_change = []
+
+    for number in range(1, len(Profit)):
+        revenue_change.append((int(Profit[number]) - int(Profit[number-1])))
     
-    #find a set of canditates and the total vote
-    candidates_set = set(candidates)    
-    total_vote = len(voterid)
+    # calculate average revenue change
+    revenue_average = sum(revenue_change) / len(revenue_change)
     
+    # calculate total length of months
+    total_months = len(months)
 
-    #create a new list with the candidates names
-    candidate_names = []
-    
-    for row in candidates_set:  
-        candidate_names.append(row)
-
-    #print the results
-    print("Election Results")
-    print("----------------------------------------")
-    print(f"Total Votes {total_vote}")
-    print("----------------------------------------")
-
-    dictionary_candidate = {}
-    candidate_count = 0
-    for row in candidate_names:
-        candidate_name = str(candidate_names[candidate_count])
-        votes = candidates.count(candidate_name)
-        votes = int(votes)
-        percentage = round(votes / total_vote * 100, 2)
-        dictionary_candidate.update({ candidate_name : votes})
-        print(f"{candidate_name}: {percentage}%  ({votes} votes)" )
-        candidate_count = candidate_count + 1
+    # greatest increase in revenue
+    greatest_increase = max(revenue_change)
+    # greatest decrease in revenue
+    greatest_decrease = min(revenue_change)
 
 
+    # print the Results
+    print("Financial Analysis")
+    print("....................................................................................")
+    print("total months: " + str(total_months))
+    print("Total: " + "$" + str(sum(Profit)))
+    print("Average change: " + "$" + str(revenue_average))
+    print("Greatest Increase in Profits: " + str(months[revenue_change.index(max(revenue_change))+1]) + " " + "$" + str(greatest_increase))
+    print("Greatest Decrease in Profits: " + str(months[revenue_change.index(min(revenue_change))+1]) + " " + "$" + str(greatest_decrease))
 
 
 
